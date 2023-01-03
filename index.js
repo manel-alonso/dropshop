eded43be6e20bc798788ff0bfdc15f9d333ca34c
manel alonso <cubeworldesp98@gmail.com>
2023-01-04 00:03:43 +0100

    about page

--- index.js
+++ index.js
@@ -19,9 +19,63 @@ var x = setInterval(function() {
   document.getElementById("countdown").innerHTML = days + ":" + (hours < 10 ? "0" + hours : hours) + ":"
   + (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds < 10 ? "0" + seconds : seconds);
 
+  document.getElementById("countdown2").innerHTML = days + ":" + (hours < 10 ? "0" + hours : hours) + ":"
+  + (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds < 10 ? "0" + seconds : seconds);
+
   // If the count down is finished, write some text 
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("countdown").innerHTML = "EXPIRED";
   }
 }, 1000);
+
+
+function leftImage() {
+  const images = document.getElementsByName("image");
+  const image1 = window.getComputedStyle(images[0]).getPropertyValue("background");
+  const image2 = window.getComputedStyle(images[1]).getPropertyValue("background");
+
+  images[0].style.background = image2;
+  images[1].style.background = image1;
+}
+
+function rightImage() {
+  const images = document.getElementsByName("image");
+  const image1 = window.getComputedStyle(images[0]).getPropertyValue("background");
+  const image2 = window.getComputedStyle(images[1]).getPropertyValue("background");
+
+  images[0].style.background = image2;
+  images[1].style.background = image1;
+}
+
+function leftImage2() {
+  const images = document.getElementsByName("image2");
+  const image1 = window.getComputedStyle(images[0]).getPropertyValue("background");
+  const image2 = window.getComputedStyle(images[1]).getPropertyValue("background");
+
+  images[0].style.background = image2;
+  images[1].style.background = image1;
+}
+
+function rightImage2() {
+  const images = document.getElementsByName("image2");
+  const image1 = window.getComputedStyle(images[0]).getPropertyValue("background");
+  const image2 = window.getComputedStyle(images[1]).getPropertyValue("background");
+
+  images[0].style.background = image2;
+  images[1].style.background = image1;
+}
+
+function cerberus() {
+  const light = document.getElementById("light");
+  const dark = document.getElementById("dark");
+
+  dark.style.display = "block";
+  light.style.display = "none";
+}
+
+function flora() {
+  console.log("i work")
+  document.getElementById("dark").style.display = "none";
+  document.getElementById("light").style.display = "block";
+}
